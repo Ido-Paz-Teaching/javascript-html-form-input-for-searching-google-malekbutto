732e0eec523f3e15cd132fb6eb34468de5c1d42b
Malek Butto <malekbutto@gmail.com>
2021-11-29 15:20:42 +0200

    Edit checkForm Function

--- formValidation.js
+++ formValidation.js
@@ -1,9 +1,56 @@
-//Please , don't remove the following code 
-if (typeof window !== 'undefined') {
-    window.onload = function() {
+//Please , don't remove the following code
+const maxWordLength = 10;
+const minResult = 1;
+const maxResult = 25;
+const forbiddenWords = ['sex','porn','violence','murder'];
+let alertArr = new Array(4);
+let msgAlert = "";
+if (typeof window !== 'undefined')
+{
+    window.onload = function()
+    {
         //update the code that handles the form submit
-        let form = null;
-        form.onsubmit =null;
+        let button = document.querySelector("button");
+        let searchField = document.getElementById("q");
+        let numField = document.getElementById("num");
+        button.onclick = checkForm;
+        // let form = null;
+        // form.onsubmit =null;
+
+        function checkForm(event)
+        {
+            if (hasForbidenWord(searchField.value))
+            {
+                alertArr[0] = `-Please don't use one of these words: ${forbiddenWords}`;
+                event.preventDefault();
+            }
+            if (hasATooLongWord(searchField.value)){
+                alertArr[1] = `-Please enter words not longer than ${maxWordLength}`;
+                event.preventDefault();
+            }
+            if (!hasChar(searchField.value)){
+                alertArr[2] = "-Please enter a word to search";
+                event.preventDefault();
+            }
+            if (!isInRange(numField.value))
+            {
+                alertArr[4] = `-Please enter number between ${minResult} & ${maxResult}`;
+                event.preventDefault();
+            }
+            for (let x in alertArr){
+                if ((alertArr[x] !== "") && (alertArr[x] !== undefined))
+                {
+                    msgAlert += alertArr[x] + "\n\n";
+                }
+            }
+            if ((!isInRange(numField.value)) || (hasForbiddenWord(searchField.value)) || (hasATooLongWord(searchField.value)) || (!hasChar(searchField.value)))
+            {
+                alert(msgAlert);
+            }
+            alertArr = new Array(4);
+            msgAlert = "";
+
+        }
     }
 }
 //
@@ -15,8 +62,8 @@ function validateQ(qValue) {
             return '*Please enter only text ' + postfix;
         else if (hasATooLongWord(qValue)) 
             return `*Please enter words not longer then ${maxWordLength} ${postfix}`;
-        else if (hasForbidenWord(qValue)) 
-            return `*Please enter words other then ${forbidenWords} ${postfix}`;
+        else if (hasForbiddenWord(qValue)) 
+            return `*Please enter words other than ${forbiddenWords} ${postfix}`;
     }
     else{
         return `*Please enter at least 2 characters ${postfix}`;
@@ -26,24 +73,23 @@ function validateQ(qValue) {
 //return a validation message in case the input is invalid
 function validateNum(numValue) {
     if (!isInRange(numValue))    
-        return `*Please enter number ${minResults} to ${maxResults} to tell google how many search results we want`;    
+        return `*Please enter number ${minResult} to ${maxResult} to tell google how many search results we want`;    
     else{
         return null;
     }
 }
 //
 function isInRange(text){
-    if ((Number(text)>=1) && (Number(text)<=25))
+    if ((Number(text)>=minResult) && (Number(text)<=maxResult))
     {
         return true;
     }
     return false;
 }
 //
-function hasForbidenWord(text) {    
-    let forbidenWords = ['sex','porn','violence','murder'];
+function hasForbiddenWord(text) {    
     let str = text;
-    for (let x of forbidenWords)
+    for (let x of forbiddenWords)
     {
         if (str.includes(x))
         {
@@ -54,7 +100,7 @@ function hasForbidenWord(text) {
 }
 // 
 function hasATooLongWord(text) {   
-    if (text.length>10)
+    if (text.length>maxWordLength)
     {
         return true;
     } 
@@ -70,7 +116,7 @@ function hasChar(text) {
 }
 //
 function isNumber(text) {
-    if ((typeof(text)=="number") || ((!isNaN(parseFloat(text))) && (!isNaN(text - 0))))
+    if ((typeof(text)=="number") || ((!isNaN(text))))
     {
         return true;
     }
